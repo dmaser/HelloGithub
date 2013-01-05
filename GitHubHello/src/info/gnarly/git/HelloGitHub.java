f2e8d6926c6fe10c2a888fba07e9b4367d2d0a73
dmaser <git@gnarly.info>
2013-01-04 17:45:12 -0800

    added hello github
    Signed-off-by: dmaser <git@gnarly.info>

---
+++ GitHubHello/src/info/gnarly/git/HelloGitHub.java
@@ -0,0 +1,12 @@
+package info.gnarly.git;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Hello GitHub!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
